8095b797d987e1edbfe95dcf4e2acc4aad3421d4
Mugen87 <michael.herzog@human-interactive.org>
2015-06-03 10:39:30 +0200

    adjustment jshint in gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,13 +39,6 @@ module.exports = function(grunt) {
 			}
 		},
 		
-		jshint : {
-			options: {
-		      curly: true,
-		    },
-			files : [ 'Gruntfile.js', '<%= srcPath %>/**/*' ]
-		},
-		
 		browserify: {
 	      dist: {
 	        src: ['<%= srcPath %>/engine/**/*.js'],
@@ -83,12 +76,11 @@ module.exports = function(grunt) {
 
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-less');
 	grunt.loadNpmTasks('grunt-autoprefixer');
 	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-browserify');
 
-	grunt.registerTask('default', [ 'browserify', 'uglify', 'less', 'autoprefixer', 'concat', 'clean' ]);
+	grunt.registerTask('default', ['browserify', 'uglify', 'less', 'autoprefixer', 'concat', 'clean' ]);
 };
