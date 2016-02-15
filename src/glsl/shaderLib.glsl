12582c82b3483fb33393f267ed25e94907e36b69
Mugen87 <michael.herzog@human-interactive.org>
2016-02-15 11:48:09 +0100

    updated glsl noise function for vec2

--- src/glsl/shaderLib.glsl
+++ src/glsl/shaderLib.glsl
@@ -78,21 +78,18 @@ float noise( in float x ) {
   *
   * @returns {float} The noise value.
   */
-float noise( in vec2 x ) {
-
-    vec2 i = floor( x );
-    vec2 f = fract( x );
-
-    // these four points are used for interpolation in 2D
-    float a = random( i );
-    float b = random( i + vec2( 1.0, 0.0 ) );
-    float c = random( i + vec2( 0.0, 1.0 ) );
-    float d = random( i + vec2( 1.0, 1.0 ) );
-
+float noise( in vec2 p )
+{
+    vec2 i = floor( p );
+    vec2 f = fract( p );
+	
     // cubic curve for interpolation
-    vec2 u = f * f * ( 3.0 - 2.0 * f );
+	vec2 u = f * f * ( 3.0 - 2.0 * f );
 
-    return mix( a, b, u.x ) + ( c - a ) * u.y * ( 1.0 - u.x ) + ( d - b ) * u.x * u.y;
+    return mix( mix( random( i + vec2( 0.0, 0.0 ) ), 
+                     random( i + vec2( 1.0, 0.0 ) ), u.x ),
+                mix( random( i + vec2( 0.0, 1.0 ) ), 
+                     random( i + vec2( 1.0, 1.0 ) ), u.x ), u.y );
 }
 
  /*
